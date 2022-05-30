5f1bb8b050beeb629de9953e51fe0d7f946fe2c1
Jahid Hossain <54525656+jahidem@users.noreply.github.com>
2022-05-30 13:38:41 +0600

    Initialized a spring boot project
    with JPA, Postgre, spring web

---
+++ src/main/java/com/herokuapp/DocLabbackend/DocLabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.herokuapp.DocLabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocLabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocLabBackendApplication.class, args);
+	}
+
+}
