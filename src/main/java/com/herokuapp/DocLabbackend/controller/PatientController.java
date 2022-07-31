004adb2802459ce991f449b7730200fa17655f82
Jahid Hossain <54525656+jahidem@users.noreply.github.com>
2022-07-31 09:59:13 +0600

    show DB query on log

--- src/main/java/com/herokuapp/DocLabbackend/controller/PatientController.java
+++ src/main/java/com/herokuapp/DocLabbackend/controller/PatientController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("patient")
+@RequestMapping("/patient")
 public class PatientController {
     @Autowired
     PatientService patientService;
