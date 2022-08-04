59a0bb59eb9f0e0d86ebb7cb1d7043d718013ce1
Jahid Hossain <54525656+jahidem@users.noreply.github.com>
2022-08-04 16:10:47 +0600

    database data(s)

---
+++ src/main/resources/static/Postgre SQL/initials.sql
@@ -0,0 +1,92 @@
+
+    alter table doctor_degrees
+       drop constraint FKsqbj0s6n7sxktag3h5jvpoiwm
+
+
+    alter table doctor_degrees
+       drop constraint FK9k2p1b0d9c115mnir48v1syox
+
+
+    drop table if exists appointment cascade
+
+
+    drop table if exists degree cascade
+
+
+    drop table if exists doctor cascade
+
+
+    drop table if exists doctor_degrees cascade
+
+
+    drop table if exists patient cascade;
+
+
+    create table appointment (
+       appointment_id  serial not null,
+        appointment_accepted boolean,
+        appointment_lab_location varchar(255),
+        appointment_time timestamp,
+        doctor_id int4,
+        patient_id int4,
+        primary key (appointment_id)
+    );
+
+
+    create table degree (
+       degree_id  serial not null,
+        name varchar(255),
+        primary key (degree_id)
+    )
+
+
+    create table doctor (
+       doctorid  serial not null,
+        doctor_clinic_name varchar(255),
+        doctor_consultency_count int4,
+        doctor_email varchar(255),
+        doctor_experience int4,
+        doctor_gender varchar(255),
+        doctor_image oid,
+        doctor_info varchar(255),
+        doctor_location varchar(255),
+        doctor_name varchar(255),
+        doctor_password varchar(255),
+        doctor_rating float4,
+        doctor_speciality varchar(255),
+        doctor_sub_district varchar(255),
+        doctor_visiting_fee int4,
+        token varchar(255),
+        primary key (doctorid)
+    )
+
+
+    create table doctor_degrees (
+       degree_id int4 not null,
+        doctor_id int4 not null,
+        primary key (degree_id, doctor_id)
+    );
+
+
+    create table patient (
+       patient_id  serial not null,
+        bill float8,
+        patient_email varchar(255),
+        patient_name varchar(255),
+        patient_password varchar(255),
+        phone_number varchar(255),
+        token varchar(255),
+        primary key (patient_id)
+    )
+
+
+    alter table doctor_degrees
+       add constraint FKsqbj0s6n7sxktag3h5jvpoiwm
+       foreign key (doctor_id)
+       references doctor
+
+
+    alter table doctor_degrees
+       add constraint FK9k2p1b0d9c115mnir48v1syox
+       foreign key (degree_id)
+       references degree
--- src/main/resources/static/Postgre SQL/insetDoctorData.sql
+++ src/main/resources/static/Postgre SQL/insetDoctorData.sql
@@ -1,14 +1,15 @@
 insert into doctor(doctor_name,
                    doctor_clinic_name,
-                   doctor_district,
+                   doctor_sub_district,
                    doctor_email,
                    doctor_experience,
                    doctor_gender,
-                   doctor_info,
                    doctor_location,
                    doctor_password,
                    doctor_speciality,
-                   doctor_visiting_fee
+                   doctor_visiting_fee,
+                   doctor_rating,
+                   doctor_info
                     )
 
             values('Remon Khan',
@@ -17,10 +18,105 @@ insert into doctor(doctor_name,
                     'remonkhan@gmail.com',
                     7,
                     'Male',
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at pharetra mauris. Phasellus aliquam eu felis sit amet dignissim. Maecenas id malesuada mauris. Morbi aliquam lectus quis tortor tincidunt facilisis. ',
                     '23/1 Dhanmondi, Mohammadpur, Dhaka',
                     '123456',
                     'Dentist',
-                    1500
+                    1500,
+                    4.3,
+                    'specify a comma-separated list of table columns. Each column consists of the column name, the kind of data that column stores, the length of data, and the column constraint.'
                     );
+
+
+insert into doctor(doctor_name,
+                   doctor_clinic_name,
+                   doctor_sub_district,
+                   doctor_email,
+                   doctor_experience,
+                   doctor_gender,
+                   doctor_location,
+                   doctor_password,
+                   doctor_speciality,
+                   doctor_visiting_fee,
+                   doctor_rating,
+                   doctor_info
+                    )
+
+            values('Amir Hossain',
+                    'Sahid Sarwardi Hospital',
+                    'Dhaka',
+                    'amirhossain@gmail.com',
+                    9,
+                    'Male',
+                    '23/1 Shamoli, Mohammadpur, Dhaka',
+                    '123456',
+                    'General Doctor',
+                    800,
+                    3.9,
+                    'specify a comma-separated list of table columns. Each column consists of the column name, the kind of data that column stores, the length of data, and the column constraint.'
+                    );
+
+
+insert into doctor(doctor_name,
+                   doctor_clinic_name,
+                   doctor_sub_district,
+                   doctor_email,
+                   doctor_experience,
+                   doctor_gender,
+                   doctor_location,
+                   doctor_password,
+                   doctor_speciality,
+                   doctor_visiting_fee,
+                   doctor_rating,
+                   doctor_info
+                    )
+
+            values('Antor Kumar',
+                    'Sylhet Medical Hospital',
+                    'Sylhet',
+                    'antorkumar@gmail.com',
+                    4,
+                    'Male',
+                    '1/3 porbot, Sylhet',
+                    '123456',
+                    'Dermatologist',
+                    1000,
+                    4.9,
+                    'specify a comma-separated list of table columns. Each column consists of the column name, the kind of data that column stores, the length of data, and the column constraint.'
+                    );
+
+insert into doctor(doctor_name,
+                   doctor_clinic_name,
+                   doctor_sub_district,
+                   doctor_email,
+                   doctor_experience,
+                   doctor_gender,
+                   doctor_location,
+                   doctor_password,
+                   doctor_speciality,
+                   doctor_visiting_fee,
+                   doctor_rating,
+                   doctor_info
+                    )
+
+            values('Punima Roy',
+                    'Rongpur Medical Hospital',
+                    'Rongpur',
+                    'purnimaroy@gmail.com',
+                    4,
+                    'Male',
+                    '1/3 porbot, Rongpur',
+                    '123456',
+                    'Homoeopath',
+                    500,
+                    2.9,
+                    'specify a comma-separated list of table columns. Each column consists of the column name, the kind of data that column stores, the length of data, and the column constraint.'
+                    );
+
+
 select * from doctor;
+
+-- see deatail about a particular column
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE table_name = 'doctor'
+AND COLUMN_NAME = 'doctor_consultency_count'
